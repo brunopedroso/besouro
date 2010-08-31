cadf4b83abf7277b599004b8d4b3955e6576ba96
Bruno Pedroso <Bruno@macasa.local>
2010-08-30 23:40:46 -0300

    a todo

--- test/athos/listeners/JavaStructureListenerTest.java
+++ test/athos/listeners/JavaStructureListenerTest.java
@@ -45,8 +45,6 @@ public class JavaStructureListenerTest {
 		
 	}
 
-
-
 	@Test
 	public void shouldGenerateARemoveEvent() {
 		
@@ -106,6 +104,8 @@ public class JavaStructureListenerTest {
 		
 		// verify event data
 		Assert.assertEquals(1, generatedActions.size());
+		
+		//TODO  do we need to diferentiate unaries from binaries refactorings?
 		
 		Action action = generatedActions.get(0);
 		Assert.assertTrue(action instanceof UnaryRefactorAction);
