5ed1467712ef314bee2028e967dff3decd59c849
Bruno Pedroso <Bruno@macasa.local>
2010-09-02 10:48:55 -0300

    second integration test passing

--- test/athos/integration/IntegrationTest.java
+++ test/athos/integration/IntegrationTest.java
@@ -20,6 +20,12 @@ import athos.listeners.mock.JUnitEventFactory;
 import athos.listeners.mock.JavaStructureChangeEventFactory;
 import athos.listeners.mock.ResourceChangeEventFactory;
 import athos.listeners.mock.WindowEventsFactory;
+import athos.model.CompilationAction;
+import athos.model.EditAction;
+import athos.model.UnitTestAction;
+import athos.model.refactor.RefactorOperator;
+import athos.model.refactor.RefactorSubjectType;
+import athos.model.refactor.UnaryRefactorAction;
 import athos.stream.EpisodeClassifierStream;
 
 public class IntegrationTest {
@@ -43,15 +49,16 @@ public class IntegrationTest {
 		meter = mock(JavaStatementMeter.class);
 		resourceListener.setTestCounter(meter);
 		winListener.setJavaMeter(meter);
+		
+		// Open file (calculates the first file metrics)
+		winListener.partOpened(WindowEventsFactory.createTestEditor(new File("TestFile.java")));
+		winListener.partOpened(WindowEventsFactory.createTestEditor(new File("ProductionFile.java")));
+
 	}
 
 	@Test 
 	public void testTDDEpisodeCategory1() throws Exception {
 	
-		// Open file (calculates the first file metrics)
-		winListener.partOpened(WindowEventsFactory.createTestEditor(new File("TestFile.java")));
-		winListener.partOpened(WindowEventsFactory.createTestEditor(new File("ProductionFile.java")));
-		
 		// Add test method
 		javaListener.elementChanged(JavaStructureChangeEventFactory.createAddMethodAction("TestFile.java", "TestFile", "aTestMethod"));
 
@@ -86,5 +93,36 @@ public class IntegrationTest {
 		
 	  }
 
+	@Test 
+	public void testTDDEpisodeCategory2() throws Exception {
+	    
+		// Add test method
+		javaListener.elementChanged(JavaStructureChangeEventFactory.createAddMethodAction("TestFile.java", "TestFile", "aTestMethod"));
+		
+		
+		// Edit on test
+		when(meter.hasTest()).thenReturn(true);
+		when(meter.getNumOfTestAssertions()).thenReturn(3);
+		when(meter.getNumOfTestMethods()).thenReturn(5);
+		resourceListener.resourceChanged(ResourceChangeEventFactory.createEditAction("TestFile.java",33));
+
+		// Compile error on test
+		resourceListener.resourceChanged(ResourceChangeEventFactory.createBuildErrorEvent("TestFile.java", "error message"));
+
+		// Work on production code
+		when(meter.hasTest()).thenReturn(false);
+		when(meter.getNumOfStatements()).thenReturn(0);
+		when(meter.getNumOfMethods()).thenReturn(0);
+		when(meter.getNumOfTestAssertions()).thenReturn(0);
+		when(meter.getNumOfTestMethods()).thenReturn(0);
+		resourceListener.resourceChanged(ResourceChangeEventFactory.createEditAction("ProductionFile.java",35));
+
+		// Unit test pass
+		junitListener.sessionFinished(JUnitEventFactory.createPassingSession("TestFile.java"));
+		
+		Assert.assertEquals(1, stream.getRecognizedEpisodes().size());
+		Assert.assertEquals("[episode] test-first 2", stream.getRecognizedEpisodes().get(0));
+	}
 	
+
 }
