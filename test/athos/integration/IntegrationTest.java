3fa83b20a2ea1694050dcbb7f5cb9ed5f5dec662
Bruno Pedroso <Bruno@macasa.local>
2010-09-02 14:45:01 -0300

    just one integration test remaining, from Hongbings original suite

--- test/athos/integration/IntegrationTest.java
+++ test/athos/integration/IntegrationTest.java
@@ -481,11 +481,11 @@ public class IntegrationTest {
 	@Test 
 	public void regressionCategory2() throws Exception {
 		
-		// TODO [rule] its a strange case without an test edit after the compilation problem :-/
 		
 		// Compile error on test
 		resourceListener.resourceChanged(ResourceChangeEventFactory.createBuildErrorEvent("TestFile.java", "error message"));
 
+		// TODO [rule] its a strange case without an test edit after the compilation problem :-/
 		// Unit test pass
 		junitListener.sessionFinished(JUnitEventFactory.createPassingSession("TestFile.java"));
 		
@@ -499,15 +499,16 @@ public class IntegrationTest {
 	@Test 
 	public void productionCategory1() throws Exception {
 		
-		// TODO [rule] its a strange case without an edit after the test failure :-/
 		
 		// Edit on production code    
 		when(meter.hasTest()).thenReturn(false);
 		when(meter.getNumOfStatements()).thenReturn(14);
 		resourceListener.resourceChanged(ResourceChangeEventFactory.createEditAction("ProductionFile.java",34));
 		
+		// TODO [rule] its a strange case without an edit after the test failure :-/
+		// we do not need this failure
 		// Unit test failue
-		junitListener.sessionFinished(JUnitEventFactory.createFailingSession("TestFile.java"));
+//		junitListener.sessionFinished(JUnitEventFactory.createFailingSession("TestFile.java"));
 
 		// Unit test pass
 		junitListener.sessionFinished(JUnitEventFactory.createPassingSession("TestFile.java"));
@@ -528,10 +529,11 @@ public class IntegrationTest {
 		when(meter.getNumOfMethods()).thenReturn(5);
 		resourceListener.resourceChanged(ResourceChangeEventFactory.createEditAction("ProductionFile.java",5));
 		
-		// Unit test failue
-		junitListener.sessionFinished(JUnitEventFactory.createFailingSession("TestFile.java"));
-		
 		// TODO [rule] its a strange case without an edit after the test failure :-/
+		// we do not need this failure
+		// Unit test failue
+//		junitListener.sessionFinished(JUnitEventFactory.createFailingSession("TestFile.java"));
+		
 
 		// Unit test pass
 		junitListener.sessionFinished(JUnitEventFactory.createPassingSession("TestFile.java"));
@@ -566,17 +568,32 @@ public class IntegrationTest {
 		
 	}
 	
+	@Test 
+	public void productionCategory3() throws Exception {
+		
+		// method increase, and size increase and LARGE byte increase
+		
+		// Edit on production code    
+		when(meter.hasTest()).thenReturn(false);
+		when(meter.getNumOfMethods()).thenReturn(5);
+		when(meter.getNumOfStatements()).thenReturn(5);
+		resourceListener.resourceChanged(ResourceChangeEventFactory.createEditAction("ProductionFile.java",133));
+
+		
+		// TODO [rule] its a strange case without an edit after the test failure :-/
+		// we do not need this failure
+		// Unit test failue
+//		junitListener.sessionFinished(JUnitEventFactory.createFailingSession("TestFile.java"));
+		
+		// Unit test pass
+		junitListener.sessionFinished(JUnitEventFactory.createPassingSession("TestFile.java"));
+		
+		Assert.assertEquals(1, stream.getRecognizedEpisodes().size());
+		Assert.assertEquals("[episode] production 3", stream.getRecognizedEpisodes().get(0));
+		
+	}
 	
 	
-//			;; Production type 3 with method increase, and size increase and byte increase
-//			(deffacts production-3-episode
-//			   (ProductionEditAction (index 1) (file Triangle.java) (methodChange 2) (statementChange 5) (byteChange 125) (duration 200))    
-//			   (UnitTestAction (index 2) (file TestTriangle.java) (errmsg  "Fix the test"))        
-//			   (UnitTestAction (index 3) (file TestTriangle.java))        
-//			(printout t (test-classifier "production" "3") crlf crlf)
-//
-
-
 	
 //			(deffacts test-first-1-episode-real
 //			   (BufferTransAction (index 1) (leavingFile Frame.java))
