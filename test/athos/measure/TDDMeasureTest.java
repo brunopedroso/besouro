028d8496169d9900c6686ef8ac159fa5ed26a4fb
Bruno Pedroso <Bruno@macasa.local>
2010-09-08 15:30:12 -0300

    one more TDDMeasure test

--- test/athos/measure/TDDMeasureTest.java
+++ test/athos/measure/TDDMeasureTest.java
@@ -10,7 +10,7 @@ public class TDDMeasureTest {
 	
 	
 	@Test
-	public void firstTest() throws Exception {
+	public void contextIndependentCase() throws Exception {
 		
 		Episode e1 = new Episode();
 		e1.setClassification("test-first", "1");
@@ -20,6 +20,30 @@ public class TDDMeasureTest {
 		
 		Episode e3 = new Episode();
 		e3.setClassification("test-last", "1");
+		
+		Episode e4 = new Episode();
+		e4.setClassification("test-first", "1");
+		
+		Episode[] episodes = new Episode[]{e1,e2,e3,e4};
+		
+		TDDMeasure measure = new TDDMeasure();
+		measure.measure(episodes);
+		
+		Assert.assertEquals(0.75, measure.getTDDPercentageByNumber(), 0.0001);
+		
+	}
+	
+	@Test
+	public void contextDependentCase() throws Exception {
+		
+		Episode e1 = new Episode();
+		e1.setClassification("test-first", "1");
+		
+		Episode e2 = new Episode();
+		e2.setClassification("refactoring", "1");
+		
+		Episode e3 = new Episode();
+		e3.setClassification("test-adition", "1");
 		
 		Episode e4 = new Episode();
 		e4.setClassification("test-first", "1");
